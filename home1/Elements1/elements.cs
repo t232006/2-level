cc7f7c4ab9dd644fa9c8b262c7da03fc6b8306a2
Tim <t232006@yandex.ru>
2020-11-17 19:28:55 +0300

    zoom is here

--- home1/Elements1/elements.cs
+++ home1/Elements1/elements.cs
@@ -18,15 +18,26 @@ namespace elements
             return pos;
         }
     }
-    
-    
-    public class asteroid: SpaceElement
+
+    enum direction {grow, less}
+    public class asteroid : SpaceElement
     {
-        
+
         protected double ang; //angle
         protected Size size;
         protected byte speed;
+        protected bool zoom;
+        byte zoomprobability=40;
         protected Bitmap image;
+        string filepicture;
+        Size limitsizeMin=new Size(4,4);
+        Size limitsizeMax=new Size(150,150);
+        direction direction;
+
+        public byte ZoomProbability 
+            { get => zoomprobability;
+            set { if ((value > 100) || (value < 0)) zoomprobability = 0; else zoomprobability = value; }
+             }
         public asteroid(Point pos, Size size, double ang, byte speed):base(pos)
         {
             this.pos = pos;
@@ -34,17 +45,35 @@ namespace elements
             this.size = size;
             this.speed = speed;
             this.ang = ang;
+            //this.zoomprobability = zp;
             
             DirectoryInfo dir = new DirectoryInfo(AppContext.BaseDirectory);
             Random rand = new Random();
             int r = rand.Next(1,4);
-            string s= dir.Parent.Parent.Parent.FullName.ToString() + @"\Resources\asteroid0" + r.ToString()+".png";
+            filepicture= dir.Parent.Parent.Parent.FullName.ToString() + @"\Resources\asteroid0" + r.ToString()+".png";
             //Image pic = Image.FromFile(s) 
-            this.image = new Bitmap(Image.FromFile(s), size);
+            this.image = new Bitmap(Image.FromFile(filepicture), size);
+            r = rand.Next(100);
+            if (r <= zoomprobability)
+            {
+                zoom = true;
+                if (r <= zoomprobability / 2) direction = direction.grow; else direction = direction.less;
+            }else zoom = false;
+            
         }
         public Bitmap draw(out Point position)
         {
-            position = pos; 
+            position = pos;
+            if (zoom)
+            {
+                if (direction==direction.grow)
+                size = new Size(size.Width + 1, size.Height + 1); else
+                size = new Size(size.Width - 1, size.Height - 1);
+                if (size.Equals(limitsizeMax)) direction = direction.less;
+                else
+                if (size.Equals(limitsizeMin)) direction = direction.grow;
+                    image = new Bitmap(Image.FromFile(filepicture),size);
+            }
             return image;
         }
         public void Move(Size size)
